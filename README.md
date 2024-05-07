6af0b13172ee22fd7e8acce5d1595960f8a60ef7
Tu Nombre <red@example.com>
2024-05-07 19:09:40 +0200

    Create filter, Count columns and rows, Compact table.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# 
 # Apiator
+
+## Mini fix
+To use the symbol "!" to without using "\\!":
+```bash
+$ cat .zshrc, .bashrc .shellrc
+setopt NO_BANG_HIST # (event history) 
+```
+
--- main.go
+++ main.go
@@ -4,8 +4,8 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
-
 	"github.com/fatih/color"
 	"github.com/jedib0t/go-pretty/v6/table"
 	"github.com/tidwall/gjson"
@@ -17,17 +17,34 @@ func check(e error) {
 	}
 }
 
-func Init() (inputMaxMachines int, inputOS string, inputPlatform string, inputDifficulty string, inputCertification string,blacklistCert string, inputName string, inputTechs string) {
+// func stringInSlice(a string, list []string) bool {
+//     for _, b := range list {
+//         if b == a {
+//             return true
+//         }
+//     }
+//     return false
+// }
+
+func capitalizeFirstLetter(s string) string {
+	if len(s) == 0 {
+		return s
+	}
+	return strings.ToUpper(string(s[0])) + s[1:]
+}
+
+
+func Init() (inputMaxMachines uint64, inputOS string, inputPlatform string, inputDifficulty string, inputCertification string, inputName string, inputTechs string) {
 	var help bool
 
-	flag.IntVar(&inputMaxMachines, "max", 10, "Maximum number of machines to display.")
+	// Yet not pointers
+	flag.Uint64Var(&inputMaxMachines, "max", 10, "Maximum number of machines to display.")
 	flag.StringVar(&inputName, "n", "-1" , "Search machine by name.")
 	flag.StringVar(&inputTechs, "t", "-1" , "Search machine by techniques.")
 	flag.StringVar(&inputDifficulty, "d", "-1", "Search machines by difficulty.")
 	flag.StringVar(&inputOS, "o", "-1", "Search machines by operating system.")
 	flag.StringVar(&inputCertification, "c", "-1", "Search machines by certifications.")
 	flag.StringVar(&inputPlatform, "p", "-1", "Search by platform.")
-	flag.StringVar(&blacklistCert, "bc", "-1", "blacklist a certification.")
 	flag.BoolVar(&help, "help", false, "Display information about usage.")
 
 	flag.Parse()
@@ -37,86 +54,73 @@ func Init() (inputMaxMachines int, inputOS string, inputPlatform string, inputDi
 		flag.PrintDefaults()
 		os.Exit(0)
 	}
+
+
+  // if strings.EqualFold("htb",inputPlatform) || strings.EqualFold("hackthebox",inputPlatform){
+		// inputPlatform = "HackTheBox"
+  // }
+
   return
-	// return inputMaxMachines, inputOS, inputPlatform, inputDifficulty, inputCertification,blac
 }
 
 func main() {
 
 
-
+  // caser := cases.Title(language.English)
 
 	// ARGUMENTS
-	inputMaxMachines, inputOS, inputPlatform, inputDifficulty, inputCertification,blacklistCert,inputName,inputTechs := Init()
-	
+	inputMaxMachines, inputOS, inputPlatform, inputDifficulty, inputCertification,inputName,inputTechs := Init()
+
+	str_MaxMachines := strconv.FormatUint(uint64(inputMaxMachines), 10)
 
 	mappedFields := map[string]string{
 		"platform" : inputPlatform,
-		"name" : inputName,
+		"name" : capitalizeFirstLetter(inputName),
 		"techniques" : inputTechs,
-		"certification" : inputCertification,
+		"certification" :inputCertification,
 		"video" : "-1",
 		"ip" : "-1",
-		"os" : inputOS,
-		"state" : inputDifficulty,
+		"os" : capitalizeFirstLetter(inputOS),
+		"state" : capitalizeFirstLetter(inputDifficulty),
 	}
 
+
 	parser:= "newData"
-		
+
 	for k, v := range mappedFields {
-        fmt.Printf("%v => %v\n",k, v)
 		if v == "-1" {
 			continue
 		}
+		if string(v[0]) == "!" {
+			parser = fmt.Sprintf("%s|#(%s!%%*%s*)#",parser,k,string(v[1:])) 
+			continue
+		}
 
-	 parser = fmt.Sprintf("%s|#(%s%%*%s*)#",parser,k,v) 
+		 parser = fmt.Sprintf("%s|#(%s%%*%s*)#",parser,k,v) 
 
   }
-
-  fmt.Printf("%s%v\n",")#10GJSON: ",parser)
-	fmt.Printf("%v",inputMaxMachines)
-
-
-	// fmt.Printf("All flags:\n")
-	// fmt.Printf("-max: %v\n",inputMaxMachines)
-	// fmt.Printf("-dif: %v\n",inputDifficulty)
-	// fmt.Printf("-os: %v\n",inputOS)
-	// fmt.Printf("-plat: %v\n",inputPlatform)
-	// fmt.Printf("-cert: %v\n",inputCertification)
-	// fmt.Printf("-bcert: %v\n",blacklistCert)
-
-
-
-	// TABLE 
-	t := table.NewWriter()
-
-	t.SetOutputMirror(os.Stdout)
-	t.AppendHeader(table.Row{"Video", "Nombre", "Plataforma", "Dificultad", "OS", "Nº Certificaciones","Certificaciones"})
 
 	dat, err := os.ReadFile("./machines.json")
 	check(err)
 	data := string(dat)
 
+	// TABLE 
+	t := table.NewWriter()
+
+	t.SetOutputMirror(os.Stdout)
+
+	n_results := gjson.Get(data, parser+"|#")
+
+	t.AppendHeader(table.Row{"Nombre / Video (" + str_MaxMachines + "/" + n_results.String() + ")", "Plataforma", "Dificultad", "OS", "Certificaciones/ Nº"})
 
 
-
-
-	parsePlatform := fmt.Sprintf("|#(platform%%*%s*)#",inputPlatform);
-	parseCertification := fmt.Sprintf("|#(certification%%*%s*)#",inputCertification);
-	parseDifficulty := fmt.Sprintf("|#(state%%*%s*)#",inputDifficulty);
-  parseOS := fmt.Sprintf("|#(os%%*%s*)#",inputOS);
-  parseBlackCertification := fmt.Sprintf("|#(certification!%%*%s*)#",blacklistCert);
-
-
-	parser = fmt.Sprintf("newData%s%s%s%s%s", parsePlatform, parseCertification, parseDifficulty, parseOS,parseBlackCertification)
-    
-	fmt.Printf("GJSON: %v\n",parser)
 
 // https://github.com/tidwall/gjson/issues/64
 	// result := gjson.Get(data, "newData.#(platform==HackTheBox)#|#(certification%*eWPT*)#|#(certification%*eWPTXv2*)#|#(certification!%*OSWE*)#")
+	// fmt.Printf("\nGJSON: %v\n",gjson.Get(data, parser+"|#"))
 
 	result := gjson.Get(data, parser)
-
+  // fmt.Printf("%v\n",result)
 
 	result.ForEach(func(key, value gjson.Result) bool {
 		name := gjson.Get(value.Raw, "name")
@@ -126,11 +130,14 @@ func main() {
 		difficulty := gjson.Get(value.Raw, "state")
 		certs := gjson.Get(value.Raw, "certification")
 		n_certs := strings.Split(certs.Raw, "\\n")
-		fmt.Printf("%T",certs)
-		// strings.Replace(certs, "fg", "FG", -1)
+		if key.Uint() == inputMaxMachines {
+			return false // stop iterating
+		}
 
+
+		
 		t.AppendRows([]table.Row{
-			{youtube_link, name, platform, difficulty, operative_system, len(n_certs), certs},
+			{ name.String() + " /\n" + youtube_link.String()  , platform, difficulty, operative_system,  strings.Join(n_certs, ",") + " / " + strconv.FormatUint(uint64(len(n_certs)), 10)},
 		})
 
 		t.AppendSeparator()
